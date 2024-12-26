769582299f1a0f0715f48c6cc07b75cc6144839d
eminamumic <emimumic1@gmail.com>
2024-12-26 17:21:56 +0100

    Zadnji

---
+++ zadatak2.js
@@ -0,0 +1,18 @@
+/*Napiši funkciju koja pronalazi najduži zajednički prefiks među nizom stringova.
+Ako ne postoji zajednički prefiks, vrati prazan string "".
+Primjer:
+Ulaz: strs = ["flower", "flow", "flight"]
+	Izlaz: "fl"
+*/
+
+function prefiks(niz) {
+  let rijec = []
+  niz.forEach((red, i) => {
+    red[i]
+  })
+  return rijec
+}
+let niz = ['flower', 'flow', 'flight']
+console.log(prefiks(niz))
+
+//Nemam vise snage
